7ec5cee3a774bb1be28df053276a87890b9bacb9
max <0972900039@mail.ru>
2025-08-04 17:44:37 +0200

    fixes

--- js/window-creator.js
+++ js/window-creator.js
@@ -9,6 +9,48 @@ export class WindowCreator {
     this.startX = 0;
     this.startY = 0;
     this.windows = [];
+    this.inputs = {
+      "Input 1": {
+        windowCount: 0,
+        color: "#000000",
+        windows: [],
+      },
+      "Input 2": {
+        windowCount: 0,
+        color: "#e7873a80",
+        windows: [],
+      },
+      "Input 3": {
+        windowCount: 0,
+        color: "#67aa1480",
+        windows: [],
+      },
+      "Input 4": {
+        windowCount: 0,
+        color: "#a319d280",
+        windows: [],
+      },
+      "Input 5": {
+        windowCount: 0,
+        color: "#1a25f180",
+        windows: [],
+      },
+      "Input 6": {
+        windowCount: 0,
+        color: "#c3848c80",
+        windows: [],
+      },
+      "Input 7": {
+        windowCount: 0,
+        color: "#2d713880",
+        windows: [],
+      },
+      "Input 8": {
+        windowCount: 0,
+        color: "#bebd7680",
+        windows: [],
+      },
+    };
     this.virtualResolutions = {
       "4k": { width: 3840, height: 2160 },
       "2k": { width: 2560, height: 1440 },
@@ -24,7 +66,6 @@ export class WindowCreator {
     this.setupEventListeners();
     this.updateWindowCount();
 
-    // Добавляем слушатели на кнопки input-btn
     document.querySelectorAll(".input-btn").forEach((btn) => {
       btn.addEventListener("click", () => {
         this.selectInput(btn.dataset.input);
@@ -37,6 +78,15 @@ export class WindowCreator {
   setupEventListeners() {
     document.getElementById("clearAll").addEventListener("click", () => {
       this.clearAllWindows();
+      const resolutionSelect = document.getElementById("virtualSize");
+      const rowsInput = document.getElementById("wallRows");
+      const colsInput = document.getElementById("wallCols");
+
+      resolutionSelect.value = "4k";
+      rowsInput.value = "1";
+      colsInput.value = "1";
+
+      resolutionSelect.dispatchEvent(new Event("change"));
     });
 
     document.getElementById("virtualSize").addEventListener("change", (e) => {
@@ -73,15 +123,54 @@ export class WindowCreator {
     outputEl.style.border = "1px solid #333";
     outputEl.style.background = "transparent";
     outputEl.style.display = "flex";
-    outputEl.style.justifyContent = "center";
-    outputEl.style.alignItems = "center";
+    outputEl.style.flexDirection = "column";
     outputEl.style.color = "#333";
     outputEl.style.fontSize = "18px";
     outputEl.style.userSelect = "none";
-    outputEl.textContent = `Output ${index}`;
     outputEl.style.zIndex = 1;
+    outputEl.style.overflow = "hidden";
 
+    //  header
+    const header = document.createElement("div");
+    header.style.padding = "6px";
+    header.style.textAlign = "center";
+    header.textContent = `Output ${index}`;
+    header.style.zIndex = 2;
+    header.style.position = "relative";
+
+    const gridOverlay = document.createElement("div");
+    gridOverlay.style.position = "absolute";
+    gridOverlay.style.top = "0";
+    gridOverlay.style.left = "0";
+    gridOverlay.style.width = "100%";
+    gridOverlay.style.height = "100%";
+    gridOverlay.style.display = "grid";
+    gridOverlay.style.gridTemplateColumns = "1fr 1fr";
+    gridOverlay.style.gridTemplateRows = "1fr 1fr";
+    gridOverlay.style.zIndex = 1;
+
+    for (let i = 0; i < 4; i++) {
+      const cell = document.createElement("div");
+
+      const row = Math.floor(i / 2);
+      const col = i % 2;
+
+      const borders = [];
+      if (col === 0) borders.push("borderRight");
+      if (row === 0) borders.push("borderBottom");
+
+      cell.style.border = "none";
+      borders.forEach((border) => {
+        cell.style[border] = "1px dashed rgba(51, 51, 51, 0.5)";
+      });
+
+      gridOverlay.appendChild(cell);
+    }
+
+    outputEl.appendChild(header);
+    outputEl.appendChild(gridOverlay);
     this.canvas.appendChild(outputEl);
+
     this.windows.push(outputEl);
     this.windowCount++;
     this.updateWindowCount();
@@ -178,8 +267,14 @@ export class WindowCreator {
 
   createWindow(x, y, width, height) {
     const windowId = "window_" + Date.now();
+    const input = this.currentInput;
 
-    const backgroundColor = this.hexToRgba("#000000", 0.4);
+    
+    this.inputs[input].windowCount++;
+    const windowNumber = this.inputs[input].windowCount;
+
+    const baseColor = this.inputs[input].color || "#000000";
+    const backgroundColor = this.hexToRgba(baseColor, 0.4);
 
     const windowEl = document.createElement("div");
     windowEl.className = "window";
@@ -190,34 +285,61 @@ export class WindowCreator {
     windowEl.style.height = height + "px";
     windowEl.style.background = backgroundColor;
     windowEl.style.zIndex = 1000;
+    windowEl.dataset.input = input;
 
-    // Добавляем data-атрибут с выбранным Input
-    windowEl.dataset.input = this.currentInput;
-
+    
     windowEl.innerHTML = `
-      <div class="window-header">
-        <span>${this.currentInput} - Window ${this.windowCount + 1}</span>
-        <div class="window-controls">
-          <button class="window-maximize" onclick="windowCreator.maximizeWindow('${windowId}')">
-            <img src="images/maximize.svg" alt="Maximize" width="16" height="16" />
-          </button>
-          <button class="window-close" onclick="windowCreator.closeWindow('${windowId}')">
-            <img src="images/close.svg" alt="Close" width="16" height="16" />
-          </button>
-        </div>
-      </div>
-      <div class="window-info" id="info_${windowId}"></div>
-      <div class="resize-handle"></div>
-    `;
-
+  <div class="window-header">
+    <span>${input}</span>
+    <div class="window-controls">
+      <button class="window-maximize" onclick="windowCreator.maximizeWindow('${windowId}')">
+        <img src="images/maximize.svg" alt="Maximize" width="16" height="16" />
+      </button>
+      <button class="window-close" onclick="windowCreator.closeWindow('${windowId}')">
+        <img src="images/close.svg" alt="Close" width="16" height="16" />
+      </button>
+    </div>
+  </div>
+  <div class="window-info" id="info_${windowId}"></div>
+  <div class="resize-handle">${windowNumber}</div>
+`;
     this.canvas.appendChild(windowEl);
     this.windows.push(windowId);
     this.windowCount++;
-    this.updateWindowCount();
 
+    const windowInfo = {
+      id: windowId,
+      number: windowNumber,
+      position: { x, y },
+      size: { width, height },
+      virtualPosition: this.calculateVirtualPosition(x, y),
+      virtualSize: this.calculateVirtualSize(width, height),
+    };
+
+    this.inputs[input].windows.push(windowInfo);
+
+    this.updateWindowCount();
     this.makeDraggable(windowEl);
     this.makeResizable(windowEl);
     this.updateWindowInfo(windowEl);
+  }
+
+  calculateVirtualPosition(x, y) {
+    const scaleX = this.totalVirtualWidth / this.canvas.clientWidth;
+    const scaleY = this.totalVirtualHeight / this.canvas.clientHeight;
+    return {
+      x: Math.round(x * scaleX),
+      y: Math.round(y * scaleY),
+    };
+  }
+
+  calculateVirtualSize(width, height) {
+    const scaleX = this.totalVirtualWidth / this.canvas.clientWidth;
+    const scaleY = this.totalVirtualHeight / this.canvas.clientHeight;
+    return {
+      width: Math.round(width * scaleX),
+      height: Math.round(height * scaleY),
+    };
   }
 
   makeDraggable(windowEl) {
@@ -309,13 +431,81 @@ export class WindowCreator {
     const windowEl = document.getElementById(windowId);
     if (!windowEl) return;
 
-    const canvasWidth = this.canvas.clientWidth;
-    const canvasHeight = this.canvas.clientHeight;
+    const windowRect = windowEl.getBoundingClientRect();
+    const canvasRect = this.canvas.getBoundingClientRect();
 
-    windowEl.style.left = "0px";
-    windowEl.style.top = "0px";
-    windowEl.style.width = canvasWidth + "px";
-    windowEl.style.height = canvasHeight + "px";
+    const outputs = Array.from(document.querySelectorAll(".output-block"));
+
+  
+    const intersectedQuadrants = [];
+
+    outputs.forEach((output) => {
+      const outputRect = output.getBoundingClientRect();
+
+     
+      if (
+        windowRect.right < outputRect.left ||
+        windowRect.left > outputRect.right ||
+        windowRect.bottom < outputRect.top ||
+        windowRect.top > outputRect.bottom
+      ) {
+        return; 
+      }
+
+     
+      const outputWidth = outputRect.width;
+      const outputHeight = outputRect.height;
+      const quadrantWidth = outputWidth / 2;
+      const quadrantHeight = outputHeight / 2;
+
+  
+      for (let row = 0; row < 2; row++) {
+        for (let col = 0; col < 2; col++) {
+          const quadrantLeft = outputRect.left + col * quadrantWidth;
+          const quadrantTop = outputRect.top + row * quadrantHeight;
+          const quadrantRight = quadrantLeft + quadrantWidth;
+          const quadrantBottom = quadrantTop + quadrantHeight;
+
+        
+          if (
+            !(
+              windowRect.right < quadrantLeft ||
+              windowRect.left > quadrantRight ||
+              windowRect.bottom < quadrantTop ||
+              windowRect.top > quadrantBottom
+            )
+          ) {
+          
+            intersectedQuadrants.push({
+              left: quadrantLeft,
+              top: quadrantTop,
+              right: quadrantRight,
+              bottom: quadrantBottom,
+            });
+          }
+        }
+      }
+    });
+
+    if (intersectedQuadrants.length === 0) return;
+
+  
+    const minLeft = Math.min(...intersectedQuadrants.map((q) => q.left));
+    const minTop = Math.min(...intersectedQuadrants.map((q) => q.top));
+    const maxRight = Math.max(...intersectedQuadrants.map((q) => q.right));
+    const maxBottom = Math.max(...intersectedQuadrants.map((q) => q.bottom));
+
+   
+    const left = minLeft - canvasRect.left;
+    const top = minTop - canvasRect.top;
+    const width = maxRight - minLeft;
+    const height = maxBottom - minTop;
+
+   
+    windowEl.style.left = left + "px";
+    windowEl.style.top = top + "px";
+    windowEl.style.width = width + "px";
+    windowEl.style.height = height + "px";
 
     this.updateWindowInfo(windowEl);
   }
@@ -323,25 +513,39 @@ export class WindowCreator {
   closeWindow(windowId) {
     const windowEl = document.getElementById(windowId);
     if (windowEl) {
+      const input = windowEl.dataset.input;
+
+      this.inputs[input].windows = this.inputs[input].windows.filter(
+        (window) => window.id !== windowId
+      );
+      this.inputs[input].windowCount = this.inputs[input].windows.length;
+
       windowEl.remove();
       this.windows = this.windows.filter((id) => id !== windowId);
       this.windowCount--;
+
       this.updateWindowCount();
     }
   }
 
   clearAllWindows() {
     this.windows.forEach((el) => {
-      if (el instanceof HTMLElement) {
-        el.remove();
-      } else if (typeof el === "string") {
-        // старые окна по id
+      if (typeof el === "string") {
         const windowEl = document.getElementById(el);
         if (windowEl) windowEl.remove();
       }
     });
+
+    document.querySelectorAll(".output-block").forEach((el) => el.remove());
+
+    for (const input in this.inputs) {
+      this.inputs[input].windowCount = 0;
+      this.inputs[input].windows = [];
+    }
+
     this.windows = [];
     this.windowCount = 0;
+
     this.updateWindowCount();
   }
 
@@ -383,25 +587,33 @@ export class WindowCreator {
     const infoEl = windowEl.querySelector(".window-info");
     if (!infoEl) return;
 
-    const canvasWidth = this.canvas.clientWidth;
-    const canvasHeight = this.canvas.clientHeight;
-
-    const scaleX = this.totalVirtualWidth / canvasWidth;
-    const scaleY = this.totalVirtualHeight / canvasHeight;
-
     const windowLeft = Number.parseFloat(windowEl.style.left);
     const windowTop = Number.parseFloat(windowEl.style.top);
     const windowWidth = Number.parseFloat(windowEl.style.width);
     const windowHeight = Number.parseFloat(windowEl.style.height);
 
-    const virtualX = Math.round(windowLeft * scaleX);
-    const virtualY = Math.round(windowTop * scaleY);
-    const virtualWidth = Math.round(windowWidth * scaleX);
-    const virtualHeight = Math.round(windowHeight * scaleY);
+    const virtualPosition = this.calculateVirtualPosition(
+      windowLeft,
+      windowTop
+    );
+    const virtualSize = this.calculateVirtualSize(windowWidth, windowHeight);
+
+ 
+    const input = windowEl.dataset.input;
+    const windowId = windowEl.id;
+    const windowInfo = this.inputs[input].windows.find(
+      (w) => w.id === windowId
+    );
+    if (windowInfo) {
+      windowInfo.position = { x: windowLeft, y: windowTop };
+      windowInfo.size = { width: windowWidth, height: windowHeight };
+      windowInfo.virtualPosition = virtualPosition;
+      windowInfo.virtualSize = virtualSize;
+    }
 
     infoEl.innerHTML = `
-    <div>Pos: ${virtualX}, ${virtualY}</div>
-    <div>Size: ${virtualWidth}×${virtualHeight}</div>
-  `;
+      <div>Pos: ${virtualPosition.x}, ${virtualPosition.y}</div>
+      <div>Size: ${virtualSize.width}×${virtualSize.height}</div>
+    `;
   }
 }
