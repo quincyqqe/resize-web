8985bece81f392353576f509c0273563ee36fb80
max <0972900039@mail.ru>
2025-08-05 19:09:32 +0200

    add constants

---
+++ js/constants.js
@@ -0,0 +1,16 @@
+export const MAX_OUTPUTS = 8;
+export const VIRTUAL_RESOLUTIONS = {
+  "4k": { width: 3840, height: 2160 },
+  "2k": { width: 2560, height: 1440 },
+};
+export const INPUT_COLORS = {
+  "Input 1": { color: "rgba(0,0,0,0.4)" },
+  "Input 2": { color: "rgba(231,135,58,0.4)" },
+  "Input 3": { color: "rgba(103,170,20,0.4)" },
+  "Input 4": { color: "rgba(163,25,210,0.4)" },
+  "Input 5": { color: "rgba(26,37,241,0.4)" },
+  "Input 6": { color: "rgba(195,132,140,0.4)" },
+  "Input 7": { color: "rgba(45,113,56,0.4)" },
+  "Input 8": { color: "rgba(190,189,118,0.4)" },
+};
+export const SYSTEM_SETTINGS_PASSWORD = "admin";
